464a86caa4a73d266fa2b8e73b2ba8486f884ee5
taiiii123 <taiiii88UQTG@gmail.com>
2025-04-29 17:49:35 +0900

    検索機能と履歴機能に関する説明を追加

--- src/extension.ts
+++ src/extension.ts
@@ -239,11 +239,34 @@ class UsageInfoNode extends TreeNode {
         super(messageTitle, vscode.TreeItemCollapsibleState.Expanded);
         this.iconPath = new vscode.ThemeIcon("info");
 
-        // 言語に基づいて使用方法の詳細を追加
-        this.addChild(new InstructionNode(l10n.t('1. Select text in the editor'), 'selection'));
-        this.addChild(new InstructionNode(l10n.t('2. All occurrences will be shown here'), 'list-tree'));
-        this.addChild(new InstructionNode(l10n.t('3. Click on an item to navigate to it'), 'go-to-file'));
-        this.addChild(new InstructionNode(l10n.t('4. Selected occurrences are highlighted'), 'symbol-color'));
+        // 検索機能に関する説明
+        const searchFeaturesNode = new TreeNode(
+            l10n.t('Search Features:'),
+            vscode.TreeItemCollapsibleState.Expanded
+        );
+        searchFeaturesNode.iconPath = new vscode.ThemeIcon('search');
+
+        searchFeaturesNode.addChild(new InstructionNode(l10n.t('1. Select text in the editor'), 'selection'));
+        searchFeaturesNode.addChild(new InstructionNode(l10n.t('2. All exact matches will be shown here'), 'list-tree'));
+        searchFeaturesNode.addChild(new InstructionNode(l10n.t('3. Click on an item to navigate to it'), 'go-to-file'));
+        searchFeaturesNode.addChild(new InstructionNode(l10n.t('4. Selected occurrences are highlighted'), 'symbol-color'));
+
+        // 履歴機能に関する説明
+        const historyFeaturesNode = new TreeNode(
+            l10n.t('History Features:'),
+            vscode.TreeItemCollapsibleState.Expanded
+        );
+        historyFeaturesNode.iconPath = new vscode.ThemeIcon('history');
+
+        historyFeaturesNode.addChild(new InstructionNode(l10n.t('1. Selections you navigate to are saved in history'), 'bookmark'));
+        historyFeaturesNode.addChild(new InstructionNode(l10n.t('2. Click the history icon to view past selections'), 'clock'));
+        historyFeaturesNode.addChild(new InstructionNode(l10n.t('3. History entries show the line content and position'), 'list-ordered'));
+        historyFeaturesNode.addChild(new InstructionNode(l10n.t('4. Click a history item to return to that location'), 'arrow-right'));
+        historyFeaturesNode.addChild(new InstructionNode(l10n.t('5. Line selection history is preserved between sessions'), 'save'));
+
+        // メインノードに追加
+        this.addChild(searchFeaturesNode);
+        this.addChild(historyFeaturesNode);
     }
 }
 
